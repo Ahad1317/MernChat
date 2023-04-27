dfe94315bdd19b2303f93315dfa1971de2c5f28e
Ahad1713 <khanazan761@gmail.com>
2023-04-27 19:19:08 +0530

    :art: reform code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-import { BrowserRouter } from "react-router-dom";
+
 
 root.render(
   <React.StrictMode>
